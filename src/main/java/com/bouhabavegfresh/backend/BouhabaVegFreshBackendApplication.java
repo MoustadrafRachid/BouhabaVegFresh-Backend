674f1595889db1e48e2ff490777635a418960c89
Moustadraf Rachid <moustadraf.rachid@gmail.com>
2024-09-05 21:31:32 +0200

    Initial commit for BouhabaVegFresh backend

---
+++ src/main/java/com/bouhabavegfresh/backend/BouhabaVegFreshBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bouhabavegfresh.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouhabaVegFreshBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BouhabaVegFreshBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bouhabavegfresh/backend/BouhabaVegFreshBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bouhabavegfresh.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouhabaVegFreshBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
